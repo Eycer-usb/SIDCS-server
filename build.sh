55a2b8161d3456bb544c9b0ce8cb10c68208c191
Eros Cedeño <16-10216@usb.ve>
2023-11-16 21:30:35 -0400

    Tryyy

--- build.sh
+++ build.sh
@@ -1,13 +1,7 @@
 echo Start building...
 
-apt -y update
-apt -y upgrade
-
-apt -y install python3
-apt -y install python3-pip
-
 npm install
 npx @nestjs/cli build
-# pip install -r scripts/requirements.txt
+
 python3 scripts/migrations/load_images.py
 ./seed.sh
